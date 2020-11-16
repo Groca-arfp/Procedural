043d47f7d4db28f49646a515fcb8f603df7308ec
Georges <72741201+Groca-arfp@users.noreply.github.com>
2020-11-16 11:56:26 +0100

    Add files via upload

---
+++ Celciustofahrenheit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace celcius_to_fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f, c;
+
+            Console.WriteLine("Saisissez la valeur en Celsius:");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            f = c * 9 / 5 + 32;
+
+            Console.WriteLine(c + "°C en  Fahrenheit est: " + f + " °F ");
+
+            Console.WriteLine("Saisissez la valeur en Fahrenheit:");
+            f = Convert.ToDouble(Console.ReadLine());
+
+            c = (f - 32) * 5 / 9;
+
+            Console.WriteLine(f + "°F en Celsius est: " + c + " °C ");
+            Console.ReadLine();
+        }
+    }
+}
