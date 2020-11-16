0ef0eeda1a626f6f832fb9c21643578eefb46ae6
Georges <72741201+Groca-arfp@users.noreply.github.com>
2020-11-16 11:57:52 +0100

    Add files via upload

---
+++ FahrenheittoCelcius.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fahrenheit_en_celcius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int celsius, faren;
+            Console.WriteLine("saisissez la température en °C : ");
+            celsius = int.Parse(Console.ReadLine());
+            faren = (celsius * 9) / 5 + 32;
+            Console.WriteLine("La conversion en °F) : " + faren);
+            Console.ReadLine();
+        }
+    }
+}
